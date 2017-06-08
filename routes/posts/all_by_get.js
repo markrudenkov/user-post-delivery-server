73e40ff342ca71b0ae7580152d7b9437ec6663ef
markrudenkov <mark.rudenkov@gmail.com>
2017-06-08 14:55:46 +0300

    logger connected to posts module

--- routes/posts/all_by_get.js
+++ routes/posts/all_by_get.js
@@ -1,6 +1,8 @@
 const data = require('../../posts_data.json');
+const logger = require('../../winston_logger');
 
-module.exports = function (req, res) {
+module.exports = function (req, res,next) {
     const posts = data;
     res.json(posts);
+    next(logger.info('posts fetchet by get method'))
 };
